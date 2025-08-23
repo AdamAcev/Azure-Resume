a5ff127238906f23b7b9dae2008854eec76ffe37
Adam <Adam.Acevedo551@gmail.com>
2025-08-23 14:51:01 -0500

    Convert nested repo 'main' to regular folder

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -2,23 +2,42 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Text;
 
 namespace Company.Function
 {
-    public class GetResumeCounter
+    public static class GetResumeCounter
     {
-        private readonly ILogger<GetResumeCounter> _logger;
-
-        public GetResumeCounter(ILogger<GetResumeCounter> logger)
-        {
-            _logger = logger;
-        }
-
+       
         [Function("GetResumeCounter")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public static HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDBInput(
+            databaseName: "AzureResume",
+            containerName: "Counter",
+            ConnectionStringSetting = "AzureResumeConnectionString",
+            Id = "1",
+            PartitionKey = "1")] Counter counter,
+            [CosmosDBOutput(
+            databaseName: "AzureResume",
+            containerName: "Counter",
+            Connection  = "AzureResumeConnectionString",
+            Id = "1",
+            PartitionKey = "1")] out Counter updatedCounter)
+        
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+
+            updatedCounter = counter;
+            updatedCounter.Count +1
+
+            var jsonToReturn = JsonConvert.SerializeObject(counter);
+
+            return new HttpResponseData(System.Net.HttpStatuscode.OK)
+            {
+                Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
+            };
         }
     }
 }
